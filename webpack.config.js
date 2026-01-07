646e428ce1947ed5e5fa5ae08f9bc00b8d3f7538
imspdr <konglsh@naver.com>
2026-01-08 03:47:12 +0900

    fix: webpack publicPath for GitHub Pages and workflow updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,71 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ModuleFederationPlugin } = require('webpack').container;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const deps = require('./package.json').dependencies;
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'docs'),
-    filename: 'bundle.js',
-    publicPath: process.env.NODE_ENV === 'production' ? '/kospi200/' : 'auto',
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            configFile: path.resolve(__dirname, 'babel.config.js'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.tsx',
+    mode: isProduction ? 'production' : 'development',
+    output: {
+      path: path.resolve(__dirname, 'docs'),
+      filename: 'bundle.js',
+      publicPath: isProduction ? '/kospi200/' : 'auto',
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'babel.config.js'),
+            },
           },
         },
-      },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'kospi200',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './App': './src/App',
+        },
+        shared: {
+          react: { singleton: true, requiredVersion: deps.react },
+          'react-dom': { singleton: true, requiredVersion: deps['react-dom'] },
+          '@emotion/react': { singleton: true, requiredVersion: deps['@emotion/react'] },
+          '@emotion/styled': { singleton: true, requiredVersion: deps['@emotion/styled'] },
+        },
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        favicon: path.resolve(__dirname, 'imspdr.png'),
+      }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'public/data', to: 'data', noErrorOnMissing: true }],
+      }),
     ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'kospi200',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/App',
+    devServer: {
+      port: 3200,
+      hot: true,
+      historyApiFallback: true,
+      static: {
+        directory: path.join(__dirname, 'public'),
       },
-      shared: {
-        react: { singleton: true, requiredVersion: deps.react },
-        'react-dom': { singleton: true, requiredVersion: deps['react-dom'] },
-        '@emotion/react': { singleton: true, requiredVersion: deps['@emotion/react'] },
-        '@emotion/styled': { singleton: true, requiredVersion: deps['@emotion/styled'] },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
       },
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: path.resolve(__dirname, 'imspdr.png'),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: 'public/data', to: 'data', noErrorOnMissing: true }],
-    }),
-  ],
-  devServer: {
-    port: 3200,
-    hot: true,
-    historyApiFallback: true,
-    static: {
-      directory: path.join(__dirname, 'public'),
     },
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
+  };
 };
